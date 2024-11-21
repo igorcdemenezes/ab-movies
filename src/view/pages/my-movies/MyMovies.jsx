8b48562e35c11c1e77d7f5c8007d0155fa10da35
igorcdemenezes <igorcdemenenezes@gmail.com>
2024-11-21 14:51:55 -0300

    feat: Listagem dos filmes populares implementada integração com a API para listar os filmes populares

--- src/view/pages/my-movies/MyMovies.jsx
+++ src/view/pages/my-movies/MyMovies.jsx
@@ -1,3 +1,48 @@
-export default function MyMovies() {
-  return <>MyMovies</>;
-}
+import { useEffect, useState } from "react";
+import styles from "./MyMovies.module.css";
+
+const MyMovies = () => {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=1`;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(API_URL);
+
+        if (!response.ok) {
+          throw new Error("Erro na requisição");
+        }
+
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (err) {
+        setError("Algo de errado aconteceu!");
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      <div>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <h3>{movie.title}</h3>
+            <div className={styles.divAvgYear}>
+              <p>Nota: {movie.vote_average}</p>
+              <p>{movie.release_date}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyMovies;
